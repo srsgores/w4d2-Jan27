a1ce090999eaed447f8dc2cd9580a56c3de4992f
root <root@rmit-coder.local>
2020-01-28 06:48:06 +0000

    Update live URL for examples

--- examples/jQuery/jquery-farter.js
+++ examples/jQuery/jquery-farter.js
@@ -1,3 +1,5 @@
+// live demo: https://codewith.seangoresht.com/preview/lectures/w4d2/examples/jQuery/jquery-farter.html
+
 jQuery(function($) {
 	// TODO: print "FART" for every time the fart button is pressed
 });
--- examples/jQuery/jquery-html.js
+++ examples/jQuery/jquery-html.js
@@ -1,3 +1,5 @@
+// live demo here: https://codewith.seangoresht.com/preview/lectures/w4d2/examples/jQuery/jquery-html.html
+
 jQuery(function($) {
 	const celebrities = ["Jonah Hill", "James Franco", "Lady GaGa", "Bradley Cooper"];
 	const celebrityFAQItemHTML = celebrities.reduce((concatenatedCelebrityNames, celebrityName) => {
